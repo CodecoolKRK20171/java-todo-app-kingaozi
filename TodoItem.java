0fe50fd5ad5542a94f14aca8f4eb62a0551246ce
Kinga Ozimirska <kinga.ozimirska@gmail.com>
2017-08-27 14:04:17 +0200

    Created TodoItem module

--- TodoItem.java
+++ TodoItem.java
@@ -1,7 +1,33 @@
-/**
- * Class representing a single todo item.
- * 
- */
 public class TodoItem{
+    private String name;
+    boolean isDone;
 
+    public TodoItem(String name){
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark () {
+        this.isDone = false;
+    }
+
+    public String getName (String name) {
+        return this.name;
+    }
+
+    public String getString () {
+
+        if (this.isDone) {
+            return  "[X] " + this.name;
+        } 
+        else {
+            return "[ ] " + this.name;
+        }
+        
+    }
+    
 }
