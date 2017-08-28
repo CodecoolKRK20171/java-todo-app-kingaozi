3ffcea4de7288b08445d54ab6eb286df8cc0a427
Kinga Ozimirska <kinga.ozimirska@gmail.com>
2017-08-28 12:19:54 +0200

    Created addItem and archiveItem methods

--- TodoList.java
+++ TodoList.java
@@ -1,7 +1,18 @@
-/**
- * Class representing a todo list.
- * TodoList object should aggregate TodoItem objects.
- */
+import java.util.ArrayList;
+
 public class TodoList{
 
+    ArrayList<TodoItem> todoList = new ArrayList<TodoItem> ();
+
+    public void addItem (TodoItem item) {
+        this.todoList.add(item);
+    }
+
+    public void archiveItem (TodoItem item) {
+        for(int i =0; i < todoList.size(); i++) {
+            if(item.getIsDone()) {
+                todoList.remove(item);
+            }
+        }
+    }
 }
