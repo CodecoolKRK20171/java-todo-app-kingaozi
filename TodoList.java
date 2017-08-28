9efbfb877014d7c5809f6f99c7f7710ae342d8b9
Kinga Ozimirska <kinga.ozimirska@gmail.com>
2017-08-28 14:29:25 +0200

    Finished TodoList module

--- TodoList.java
+++ TodoList.java
@@ -4,11 +4,14 @@ public class TodoList{
 
     ArrayList<TodoItem> todoList = new ArrayList<TodoItem> ();
 
-    public void addItem (TodoItem item) {
-        this.todoList.add(item);
+    public void addItem (String item) {
+        
+        TodoItem task= new TodoItem(item);
+        this.todoList.add(task);
     }
 
     public void archiveItem () {
+
         for(int i =0; i < todoList.size(); i++) {
             if(todoList.get(i).getIsDone()) {
                 todoList.remove(i);
@@ -17,13 +20,21 @@ public class TodoList{
     }
 
     public void markItem(String chosenItem) {
+
         for (TodoItem item: todoList) {
             if(item.getItemName().equals(chosenItem)) {
                 item.mark();
             }
-            else {
-                System.out.println("There is no such option!");
-            }
+        }
+    }
+
+    public void getItemsList () {
+
+        int index = 1;
+
+        for(TodoItem item: todoList) {
+            System.out.println("(" + index + ")" + item.showString());
+            index ++;
         }
     }
 }
