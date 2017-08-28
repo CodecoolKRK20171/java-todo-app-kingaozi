74c04b7a08b85bb20719b0a983d42bbb8c33caff
Kinga Ozimirska <kinga.ozimirska@gmail.com>
2017-08-28 14:29:46 +0200

    Finished App module

--- App.java
+++ App.java
@@ -1,9 +1,54 @@
-/**
- * Class representing the TodoApp application.
- * It is the main entry point for this program.
- */
+import java.util.Scanner;
+
 public class App{
+
+    static TodoList listOfTodoItems = new TodoList();
+  
   public static void main(String[] args) {
 
+      boolean isWorking = true;
+
+      while (isWorking) {
+
+          showMenu();
+          Scanner reader = new Scanner(System.in);
+          String userInput = reader.nextLine();
+
+          if (userInput.equals("1")) {
+              listOfTodoItems.getItemsList();
+          }
+
+          else if (userInput.equals("2")) {
+              System.out.println("Enter the task name you want to add to the list: ");
+              String task =reader.nextLine();
+              listOfTodoItems.addItem(task);
+          }
+          else if (userInput.equals("3")) {
+            System.out.println("Enter the task name you want to mark: ");
+            Scanner scanner = new Scanner(System.in);
+            String itemToMark = scanner.nextLine();
+            listOfTodoItems.markItem(itemToMark);
+          }
+          else if (userInput.equals("4")) {
+              listOfTodoItems.archiveItem();
+          }
+          else if (userInput.equals("0")) {
+              isWorking = false;
+          }
+
+      }
+  }
+
+  public static void showMenu () {
+      String menuOptions = String.format("%s\n%s\n%s\n%s\n%s\n%s\n",
+          "\nChose an option:\n",
+          "(1) show To Do List",
+          "(2) add task to To Do List",
+          "(3) mark task",
+          "(4) archive tasks",
+          "(0) Exit");
+
+      System.out.print(menuOptions);
   }
 }
+
